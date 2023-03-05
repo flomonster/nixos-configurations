ea31427b030c39731c0c3a05b0adebd1cc71a0bf
Florian Amsallem <florian.amsallem@epita.fr>
2023-03-05 16:55:29 +0100

    cave: add virtual camera support for obs

--- cave/configuration.nix
+++ cave/configuration.nix
@@ -42,6 +42,16 @@
   boot.cleanTmpDir = true;
   boot.tmpOnTmpfs = true;
 
+  # Add kernel modules
+  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback.out ];
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModprobeConfig = ''
+    # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
+    # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
+    # https://github.com/umlaeute/v4l2loopback
+    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+  '';
+
   # Networking settings
   networking.hostName = "cave"; # Define your hostname.
   networking.networkmanager.enable = true;
