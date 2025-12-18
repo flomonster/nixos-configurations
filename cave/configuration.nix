16ff8be006e66bad735f6c07f8e259800ba385dc
Florian Amsallem <florian.amsallem@gmail.com>
2025-12-18 10:10:58 +0100

    adapt tuigreet

--- cave/configuration.nix
+++ cave/configuration.nix
@@ -78,7 +78,7 @@
   services.greetd = {
     enable = true;
     settings.default_session = {
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --time --cmd sway";
+      command = "${pkgs.tuigreet}/bin/tuigreet --remember --time --cmd sway";
       user = "greeter";
     };
   };
