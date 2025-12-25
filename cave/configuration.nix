39a002b9724d322acdb2b21cfb7553aaf5b0ea2b
Florian Amsallem <florian.amsallem@gmail.com>
2025-12-25 15:42:20 +0100

    add slurp to default packages

--- cave/configuration.nix
+++ cave/configuration.nix
@@ -164,6 +164,7 @@
     wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
     mako # notification system developed by swaywm maintainer
     adwaita-icon-theme
+    slurp
   ];
 
   # Add docker
